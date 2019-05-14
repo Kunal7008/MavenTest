77e795f187a0638bb53aaf5f61c547b7d2cfb832
Kunal7008 <kunal.bhatta513@gmail.com>
2019-05-14 11:06:17 +0530

    first commit

---
+++ src/test/java/com/selenium/coding/Run.java
@@ -0,0 +1,16 @@
+package com.selenium.coding;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Run {
+@Test
+public void get() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\KUNAL BHATTA\\Desktop\\chromedriver.exe");
+	WebDriver driver= new ChromeDriver();
+	driver.get("https://jio.com");
+	String s=driver.getTitle();
+	System.out.println(s);	
+}
+}
